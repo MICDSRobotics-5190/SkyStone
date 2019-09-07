5e795ec73b725b514c98f824b8a048ad53ad779d
Nick Clifford <nick@nickclifford.me>
2019-09-07 14:38:55 -0500

    add robotplus dependency

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/SkyStoneRobot.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.components;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.micdsrobotics.robotplus.hardware.MecanumDrive;
+import org.firstinspires.ftc.micdsrobotics.robotplus.hardware.Robot;
+
+public class SkyStoneRobot extends Robot<MecanumDrive> {
+    @Override
+    public void initHardware(HardwareMap hardwareMap) {
+        // TODO
+    }
+
+    @Override
+    public double voltageToDistance(double voltage) {
+        // TODO
+        return 0;
+    }
+}
