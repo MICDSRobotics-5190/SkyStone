63180cc7eb978f1f171765759d03e9dcce11e9b5
Nick Clifford <nick@nickclifford.me>
2020-01-21 16:20:00 -0600

    add tensorflow to blue side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueAuto.java
@@ -7,14 +7,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.micdsrobotics.robotplus.autonomous.TimeOffsetVoltage;
 import org.firstinspires.ftc.micdsrobotics.robotplus.hardware.MecanumDrive;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.components.FoundationHook;
 import org.firstinspires.ftc.teamcode.components.Grabber;
 import org.firstinspires.ftc.teamcode.components.Lift;
 import org.firstinspires.ftc.teamcode.components.Plow;
 import org.firstinspires.ftc.teamcode.components.SkyStoneRobot;
 
+import java.util.List;
+
 @Autonomous(name = "Blue")
 public class BlueAuto extends LinearOpMode {
+
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String STONE_LABEL = "Stone";
+    private static final String SKYSTONE_LABEL = "Skystone";
+
+    private static final String VUFORIA_KEY = "Abqz+2L/////AAABma0eOZ1qn0JtoRRtYBon9SdCW2pl+UV/udFm5jGdYGCtEwQGIrRVlelT+8l+8a9loKRX9hN58JSJgQ6XTHzFz7FfECsfDmQpAm2IHWxgA/rRgk4X4TxZV86koMTTIz7bL8ixS2SLqlOwB6qcesZHtJtaIPx5sUsy5rFyXFAQe7foPxD1wcedwAZb92319WcFK+j+dlH1bZkqzQ0OAacAOBs5FThNTM6DwHVzFtgMVo3bCyySE9v1s3XRmN8E0TKBBn4560Tp1YaJOtwfK9jSHiMzjl4Ou1f0azFKbUgPiQax96FmMw/BqereyseL02MvUjL7Lvp8R9a8VINQMZ0RnmQ+S//lbeXMLKLt8SHJ8weV";
+
+    private TFObjectDetector tfod;
+
+    private enum Position {
+        LEFT,
+        MIDDLE,
+        RIGHT
+    }
 
     private SkyStoneRobot robot = new SkyStoneRobot();
     private MecanumDrive drivetrain;
@@ -34,6 +55,16 @@ public class BlueAuto extends LinearOpMode {
         foundationHook = new FoundationHook(hardwareMap);
 
         waitForStart();
+
+        switch (getSkyStonePosition()) {
+            case LEFT:
+                moveDistanceCm(MecanumDrive.Direction.LEFT, 15);
+                break;
+            case RIGHT:
+                moveDistanceCm(MecanumDrive.Direction.RIGHT, 15);
+                break;
+            // default position is middle, no need to move
+        }
 
         //drive forward
         lift.raise();
@@ -125,6 +156,45 @@ public class BlueAuto extends LinearOpMode {
         if (distance <= 0) return;
         drivetrain.complexDrive(direction.angle(), velocity, 0);
         sleepDistance(distance);
+    }
+
+    private void initTensorFlow() {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, STONE_LABEL, SKYSTONE_LABEL);
+
+        tfod.activate();
+    }
+
+    private Position getSkyStonePosition() {
+        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+        if (updatedRecognitions != null) {
+            for (Recognition recognition : updatedRecognitions) {
+                if (!recognition.getLabel().equals(SKYSTONE_LABEL)) continue;
+
+                float recogCenter = (recognition.getLeft() + recognition.getRight()) / 2;
+                float positionRatio = recogCenter / recognition.getImageWidth();
+
+                if (positionRatio >= 0.66) {
+                    return Position.RIGHT;
+                }
+
+                if (positionRatio <= 0.33) {
+                    return Position.LEFT;
+                }
+            }
+        }
+        return Position.MIDDLE;
     }
 
 }
